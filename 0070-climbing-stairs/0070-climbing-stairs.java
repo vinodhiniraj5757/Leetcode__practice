83ea9bf955dab7bd3674355687bfe126081c8201
Vinodhini <151066006+vinodhiniraj5757@users.noreply.github.com>
2025-06-06 15:19:20 +0530

    Time: 0 ms (100%), Space: 39.7 MB (99.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=3)
+        return n;
+        int a=2,b=3;
+        int c=0;
+        for(int i=4;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            
+        }
+        return c;
+        
+    }
+}
