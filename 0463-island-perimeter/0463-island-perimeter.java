6a79b524680b203159a8daaea3991144f0410cb5
Vinodhini <151066006+vinodhiniraj5757@users.noreply.github.com>
2025-06-08 19:05:37 +0530

    Time: 4 ms (99.8%), Space: 45.7 MB (70.76%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int co=0;
+        // boolean b[]=new boolean[grid.length];
+        // boolean c[]=new boolean[grid[0].length];
+        for(int i=0;i<grid.length;i++)
+        {
+            
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    co=co+4;
+                     if (i > 0 && grid[i - 1][j] == 1) {
+                        co -= 2;
+                    }
+
+                    // If land to the left exists, subtract 2 (shared edge)
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        co -= 2;
+                    }
+                }
+            }
+            
+           
+        }
+       
+        return co;
+ 
+       
+        
+    }
+}
